3bf98d6006b6d58234fb0fd2504f6f65ef3a2ec5
Maung Htike <maunghtike1999@gmail.com>
2022-06-08 05:49:57 -0700

    add a tags to cards

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Contacts from "./components/Contacts";
 import logo from "./assets/logo.png";
 import resume from "./assets/resume.pdf";
 import background from "./assets/background.png";
-//import { Link } from "react-router-dom";
 
 const bg = {
   backgroundImage: `url(${background})`,
@@ -56,20 +55,20 @@ export default function App() {
       <footer className="uk-container uk-margin-large-top uk-text-center" style={footerStyle}>
         <div className="contactsContents">
 
-          {/* <Link to="https://github.com/Sfzmango" target="_blank"> */}
-          <span
-            uk-icon="icon: github-alt; ratio: 1.5"></span>
-          {/* </Link> */}
+          <a href="https://github.com/Sfzmango" target="_blank" rel="noreferrer" style={footerStyle}>
+            <span
+              uk-icon="icon: github-alt; ratio: 1.5"></span>
+          </a>
 
-          {/* <Link to="https://twitter.com/Sfzm_" target="_blank"> */}
-          <span
-            uk-icon="icon: twitter; ratio: 1.5"></span>
-          {/* </Link> */}
+          <a href="https://twitter.com/Sfzm_" target="_blank" rel="noreferrer" style={footerStyle} className="uk-margin-large-left uk-margin-large-right">
+            <span
+              uk-icon="icon: twitter; ratio: 1.5"></span>
+          </a>
 
-          {/* <Link to="https://www.linkedin.com/in/maung-htike-482b4b143/" target="_blank"> */}
-          <span
-            uk-icon="icon: linkedin; ratio: 1.5"></span>
-          {/* </Link> */}
+          <a href="https://www.aedin.com/in/maung-htike-482b4b143/" target="_blank" rel="noreferrer" style={footerStyle}>
+            <span
+              uk-icon="icon: linkedin; ratio: 1.5"></span>
+          </a>
 
         </div>
         <p>- © Maung Htike, 2022 MIT. Made using the UIkit CSS framework -</p>
@@ -77,106 +76,3 @@ export default function App() {
     </div>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-import React from "react";
-import RenderPage from "./components/RenderPage";
-import logo from "./assets/logo.png";
-import resume from "./assets/resume.pdf";
-import background from "./assets/background.png";
-//import { Link } from "react-router-dom";
-
-const bg = {
-  backgroundImage: `url(${background})`,
-  backgroundSize: "cover"
-}
-
-const navStyle = {
-  background: "#222222",
-};
-
-const footerStyle = {
-  color: "black"
-};
-
-export default function App() {
-  
-  const [page, setPage] = useState(["AboutMe", "Projects", "Contacts"]);
-
-  return (
-    <div style={bg}>
-
-      <header>
-        <nav className="uk-width-1-1\@s uk-background-cover" uk-navbar="uk-navbar" style={navStyle}>
-          <div className="uk-navbar-left">
-            <ul className="uk-margin-small-left uk-navbar-nav">
-              <li><img src={logo} alt="website logo" width="100px" /></li>
-            </ul>
-          </div>
-
-          <div className="uk-navbar-right">
-            <ul className="uk-navbar-nav uk-margin-medium-right">
-              <a href="#">
-                <span uk-icon="icon: menu; ratio: 1.5" className="uk-text-muted"></span>
-              </a>
-              <div className="uk-navbar-dropdown">
-                <ul className="uk-nav uk-navbar-dropdown-nav">
-                  <li>About Me</li>
-                  <li>Projects</li>
-                  <li>Contact</li>
-                  <li><a href={resume} target="_blank" rel="noreferrer">Resume</a></li>
-                </ul>
-              </div>
-            </ul>
-          </div>
-        </nav>
-      </header>
-
-      <RenderPage />
-
-      <footer className="uk-container uk-margin-large-top uk-text-center" style={footerStyle}>
-        <div className="contactsContents">
-
-          {/* <Link to="https://github.com/Sfzmango" target="_blank"> *//*}
-<span
-uk-icon="icon: github-alt; ratio: 1.5"></span>
-{/* </Link> *//*}
-
-{/* <Link to="https://twitter.com/Sfzm_" target="_blank"> *//*}
-<span
-  uk-icon="icon: twitter; ratio: 1.5"></span>
-{/* </Link> *//*}
-
-{/* <Link to="https://www.linkedin.com/in/maung-htike-482b4b143/" target="_blank"> *//*}
-<span
-  uk-icon="icon: linkedin; ratio: 1.5"></span>
-{/* </Link> *//*}
-
-</div>
-<p>- © Maung Htike, 2022 MIT. Made using the UIkit CSS framework -</p>
-</footer>
-</div>
-);
-}
-*/
-
-
-
-
