95d527e592581866d617e5a8a6e7a07ba85384d1
Maung Htike <maunghtike1999@gmail.com>
2022-06-08 02:30:22 -0700

    add resume and placeholder containers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
-import Page from "./components/Page";
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
+import Contacts from "./components/Contacts";
 import logo from "./assets/logo.png";
+import resume from "./assets/resume.pdf";
 //import { Link } from "react-router-dom";
 
 const navStyle = {
@@ -14,6 +17,7 @@ const footerStyle = {
 export default function App() {
   return (
     <div>
+
       <header>
         <nav className="uk-width-1-1\@s uk-background-cover" uk-navbar="uk-navbar" style={navStyle}>
           <div className="uk-navbar-left">
@@ -29,36 +33,20 @@ export default function App() {
               </a>
               <div className="uk-navbar-dropdown">
                 <ul className="uk-nav uk-navbar-dropdown-nav">
-                  <li>Home</li>
-                  <li>Experiences</li>
+                  <li>About Me</li>
                   <li>Projects</li>
-                  <li>Resume</li>
-                  <li>
-                    {/* <Link to="https://github.com/Sfzmango" target="_blank"> */}
-                    <span
-                      uk-icon="icon: github-alt; ratio: 1.5"></span>
-                    {/* </Link> */}
-                  </li>
-                  <li>
-                    {/* <Link to="https://twitter.com/Sfzm_" target="_blank"> */}
-                    <span
-                      uk-icon="icon: twitter; ratio: 1.5"></span>
-                    {/* </Link> */}
-
-                  </li>
-                  <li>
-                    {/* <Link to="https://www.linkedin.com/in/maung-htike-482b4b143/" target="_blank"> */}
-                    <span
-                      uk-icon="icon: linkedin; ratio: 1.5"></span>
-                    {/* </Link> */}
-                  </li>
+                  <li>Contact</li>
+                  <li><a href={resume} target="_blank" rel="noreferrer">Resume</a></li>
                 </ul>
               </div>
             </ul>
           </div>
         </nav>
       </header>
-      <Page />
+
+      <AboutMe />
+      <Projects />
+      <Contacts />
       <div className="uk-container uk-margin-large-top uk-text-center" style={footerStyle}>
         <p>- © Maung Htike, 2022 MIT. Made using the UIkit CSS framework -</p>
       </div>
