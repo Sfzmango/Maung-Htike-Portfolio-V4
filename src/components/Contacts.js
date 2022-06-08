d2ec6dabb4f8529ad3494d74822553923c6bc6fb
Maung Htike <maunghtike1999@gmail.com>
2022-06-08 06:25:37 -0700

    add handler for onblur to change textbox color

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,6 +2,20 @@ import React from 'react';
 
 export default function Contacts() {
 
+    function handleSubmit(e) {
+        e.preventDefault();
+        alert("Form Successful!")
+    }
+
+    function handleBlur(e) {
+        if (e.target.value) {
+            e.target.classList.add("uk-form-success");
+        }
+        else {
+            e.target.classList.add("uk-form-danger");
+        }
+    }
+
     return (
         <div className="uk-card uk-card-default uk-card-body uk-width-1-2 uk-margin-large uk-margin-large-left">
             <form className="uk-container">
@@ -10,18 +24,18 @@ export default function Contacts() {
                     <legend className="uk-legend">Contact Me</legend>
 
                     <div className="uk-margin">
-                        <input className="uk-input" type="text" placeholder="Name" />
+                        <input className="uk-input" onBlur={handleBlur} id="name" type="text" placeholder="Name" />
                     </div>
 
                     <div className="uk-margin">
-                        <input className="uk-input" type="text" placeholder="Email" />
+                        <input className="uk-input" onBlur={handleBlur} id="email" type="text" placeholder="Email" />
                     </div>
 
                     <div className="uk-margin">
-                        <textarea className="uk-textarea" rows="5" placeholder="Message"></textarea>
+                        <textarea className="uk-textarea" onBlur={handleBlur} id="message" rows="5" placeholder="Message"></textarea>
                     </div>
 
-                    <button className="uk-button uk-button-default uk-align-right">Submit</button>
+                    <button className="uk-button uk-button-default uk-align-right" type="submit" onClick={handleSubmit}>Submit</button>
                 </fieldset>
             </form>
         </div>
