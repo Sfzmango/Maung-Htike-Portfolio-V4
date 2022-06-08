a1eb3d5aef34030c9686cdcc371d43cd84375aa8
Maung Htike <maunghtike1999@gmail.com>
2022-06-08 01:13:07 -0700

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Footer() {
+
+    const footerStyle = {
+        color: "black"
+    };
+
+    <div className="uk-container uk-margin-large-top uk-text-center" style={footerStyle}>
+        <p>- © Maung Htike, 2022 MIT. Made using the UIkit CSS framework -</p>
+    </div>
+}
